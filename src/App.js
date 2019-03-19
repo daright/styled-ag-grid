e54f07af59a511739557b5178a7ac5c2357b2979
Piotr Kasperek <piotr.kasperek@pragmasoft.pl>
2019-03-19 11:29:53 +0100

    styled ag grid

--- src/App.js
+++ src/App.js
@@ -1,28 +1,71 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {AgGridReact} from 'ag-grid-react';
+import 'ag-grid-community/dist/styles/ag-grid.css';
+import 'ag-grid-community/dist/styles/ag-theme-balham.css';
+import styled from 'styled-components';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+            columnDefs: [
+                {
+                    headerName: 'Make',
+                    field: 'make',
+                },
+                {
+                    headerName: 'Model',
+                    field: 'model',
+                },
+                {
+                    headerName: 'Price',
+                    field: 'price',
+                },
+            ],
+            rowData: [
+                {
+                    make: 'Toyota',
+                    model: 'Celica',
+                    price: 35000,
+                },
+                {
+                    make: 'Ford',
+                    model: 'Mondeo',
+                    price: 32000,
+                },
+                {
+                    make: 'Porsche',
+                    model: 'Boxter',
+                    price: 72000,
+                },
+            ],
+        };
+    }
+
+    render() {
+        return (
+            <StyledDiv>
+                <div
+                    className="ag-theme-balham"
+                    style={{
+                        height: '500px',
+                        width: '600px',
+                    }}
+                >
+                    <AgGridReact columnDefs={this.state.columnDefs} rowData={this.state.rowData} />
+                </div>
+            </StyledDiv>
+        );
+    }
 }
+
+const StyledDiv = styled.div`
+    .ag-header-cell-label {
+        background-color: red;
+        color: blue;
+    }
+`;
 
 export default App;
